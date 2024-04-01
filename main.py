fb7848af1ad2a67752093b8a3891cc59096302e9
hundodayso <solder-elision.0y@icloud.com>
2024-04-01 14:00:54 +0100

    1

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
 from tkinter import *
-
+from tkinter import messagebox
+import random
+import pyperclip
 FONT_NAME = "Arial"
 FONT_SIZE = 10
+
+hello = "leng"
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8,10)
+    nr_symbols = random.randint(2,4)
+    nr_numbers = random.randint(2,4)
+
+
+
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+    password_list = password_letters + password_symbols + password_numbers
+
+    random.shuffle(password_list)
+
+    password =  "".join(password_list)
+    password_input.delete(0, END)
+    password_input.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD -------------------------------- #
 def save():
@@ -11,11 +40,17 @@ def save():
         username = username_input.get()
         password = password_input.get()
         #string = str(website_input.get()) + ' | ' + str(username_input.get()) + ' | ' + str(password_input.get()) + '\n'
+        if len(website) == 0 or len(username) == 0 or len(password) == 0:
+            messagebox.showwarning(title="Blank fields!", message="Please do not leave any blank fields!")
 
-        data_file.write(f"{website} | {username} | {password} \n")
-        website_input.delete(0, END)
-        username_input.delete(0, END)
-        password_input.delete(0, END)
+        else:
+            is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {username}"
+                                                          f"\nPassword: {password} \n\n Is it ok to save?")
+            if is_ok:
+                data_file.write(f"{website} | {username} | {password}\n")
+                website_input.delete(0, END)
+                username_input.delete(0, END)
+                password_input.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -54,7 +89,7 @@ password_label.grid(column=0, row=3, sticky=E)
 password_input = Entry(width=21)
 password_input.grid(column=1, row=3, sticky=N+E+S+W)
 
-generate_button = Button(text="Generate Password", font=(FONT_NAME, FONT_SIZE, "bold"))
+generate_button = Button(text="Generate Password", font=(FONT_NAME, FONT_SIZE, "bold"), command=generate_password)
 generate_button.grid(column=2, row=3, sticky=W)
 
 ##Add button
